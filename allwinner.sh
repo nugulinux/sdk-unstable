2deceb90f3eb955b67c335c0eff33e498795b082
Inho Oh <inho.oh@sk.com>
2019-12-06 17:57:52 +0900

    add allwinner build
    Signed-off-by: Inho Oh <inho.oh@sk.com>

---
+++ allwinner.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#set -e
+
+echo "Repository slug = ${REPOSLUG}"
+echo "SHA = ${SHA}"
+echo "Short SHA = ${SHORTSHA}"
+
+echo "Clone the repository"
+git clone https://github.com/${REPOSLUG} --recursive
+cd ${REPO}
+git checkout ${SHA}
+git reset --hard
+rm -rf .git
+
+# Fix permissions for docker
+chmod 777 $PWD
+
+cat <<EOF > sdkbuild.sh
+#!/bin/zsh
+set -e
+
+mkdir -p /mnt/UDISK
+
+rm -rf build
+mkdir build
+cd build
+
+cmake .. -DCMAKE_INSTALL_PREFIX=/mnt/UDISK -DCMAKE_TOOLCHAIN_FILE=/opt/toolchain/Toolchain.cmake
+make -j6
+make install
+mv /mnt/UDISK ./SDK
+tar cvfz files_allwinner.tgz SDK
+EOF
+chmod 755 sdkbuild.sh
+
+echo "Build crosscompile"
+docker run -t --rm -v $PWD:$PWD -w $PWD \
+    nugulinux/devenv:allwinner ./sdkbuild.sh
+
+mkdir /tmp/result
+cp build/files_allwinner.tgz /tmp/result/
+
+echo "Completed"
