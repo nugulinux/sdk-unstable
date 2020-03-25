146ffe73f1052f0c2556fa424f4fe857ecfdbc34
Inho Oh <inho.oh@sk.com>
2020-03-25 11:29:18 +0900

    add allwinner deploy scripts
    Signed-off-by: Inho Oh <inho.oh@sk.com>

---
+++ allwinner.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+echo "> Repository slug = ${REPOSLUG}"
+echo "> SHA = ${SHA}"
+echo "> Short SHA = ${SHORTSHA}"
+
+echo "> Clone the repository"
+git clone https://github.com/${REPOSLUG} --recursive
+cd ${REPO}
+git checkout ${SHA}
+git reset --hard
+rm -rf .git
+
+# Fix permissions for docker
+chmod 777 $PWD
+
+cat <<EOF > sdkbuild.sh
+#!/bin/zsh
+set -e
+
+mkdir -p /mnt/UDISK
+
+rm -rf build
+mkdir build
+cd build
+
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=/mnt/UDISK \
+    -DCMAKE_INSTALL_LIBDIR=/mnt/UDISK/lib \
+    -DCMAKE_TOOLCHAIN_FILE=/opt/toolchain/Toolchain.cmake
+make -j6
+make install
+mv /mnt/UDISK ./SDK
+tar cvfz files_allwinner_${SHORTSHA}.tgz SDK
+EOF
+chmod 755 sdkbuild.sh
+
+echo "> Build crosscompile"
+docker run -t --rm -v $PWD:$PWD -w $PWD \
+    nugulinux/devenv:allwinner ./sdkbuild.sh
+
+mkdir /tmp/result
+cp build/files_*.tgz /tmp/result/
+
+cd ..
+rm -rf ${REPO}
+
+echo "> Completed"
